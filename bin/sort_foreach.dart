96d5d516d46a746ba9da8d907598d16b68034a14
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-20 23:54:53 +0700

    sort_foreach Compleated

--- bin/sort_foreach.dart
+++ bin/sort_foreach.dart
@@ -12,14 +12,24 @@ void main(List<String> arguments) {
   ];
 
   // Integer
-  //persons.sort((p1, p2) => p1.age - p2.age);
+  persons.sort((p1, p2) => p1.age - p2.age);
 
   // String
-  //persons.sort((p1, p2) => p1.role.compareTo(p2.role));
+  persons.sort((p1, p2) => p1.role.compareTo(p2.role));
 
+  // Custom Sort
   persons.sort((p1, p2) {
     if (p1.role.compareTo(p2.role) != 0) {
       return p1.role.compareTo(p2.role);
+    } else {
+      return p1.age.compareTo(p2.age);
+    }
+  });
+
+  // Custom Sorth + roleWeight
+  persons.sort((p1, p2) {
+    if (p1.roleWeight - p2.roleWeight != 0) {
+      return p1.roleWeight - p2.roleWeight;
     } else {
       return p1.age.compareTo(p2.age);
     }
@@ -35,4 +45,15 @@ class Person {
   final int age;
 
   Person(this.role, this.age);
+
+  int get roleWeight {
+    switch (role) {
+      case 'Merchant':
+        return 1;
+      case 'Administrator':
+        return 2;
+      default:
+        return 3;
+    }
+  }
 }
