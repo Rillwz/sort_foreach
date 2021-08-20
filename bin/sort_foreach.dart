fe2fc2f0e77d31e9df039193e01d2c0a02b072c6
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-20 23:46:42 +0700

    Add more items to persons

--- bin/sort_foreach.dart
+++ bin/sort_foreach.dart
@@ -1,18 +1,32 @@
-import 'package:sort_foreach/sort_foreach.dart' as sort_foreach;
-
 void main(List<String> arguments) {
-  print('Hello world: ${sort_foreach.calculate()}!');
-
   var persons = <Person>[
     Person('Administrator', 32),
-    Person('Supplier', 16),
-    Person('Operator', 18)
+    Person('User', 52),
+    Person('Administrator', 20),
+    Person('Merchant', 52),
+    Person('Administrator', 23),
+    Person('User', 22),
+    Person('Merchant', 22),
+    Person('User', 12),
+    Person('Merchant', 55)
   ];
 
-  persons.sort();
+  // Integer
+  //persons.sort((p1, p2) => p1.age - p2.age);
+
+  // String
+  //persons.sort((p1, p2) => p1.role.compareTo(p2.role));
+
+  persons.sort((p1, p2) {
+    if (p1.role.compareTo(p2.role) != 0) {
+      return p1.role.compareTo(p2.role);
+    } else {
+      return p1.age.compareTo(p2.age);
+    }
+  });
 
   persons.forEach((element) {
-    print(element.role + '-' + element.age.toString());
+    print(element.role + ' - ' + element.age.toString());
   });
 }
 
